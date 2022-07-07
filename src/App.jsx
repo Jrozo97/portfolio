52ec1d261110d839045d959420788e96fe2a70f0
Jimmy Rozo <jimmyrozo29@gmail.com>
2022-07-07 16:14:55 -0500

    V0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./common/header/Header";
+import Footer from "./common/footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
